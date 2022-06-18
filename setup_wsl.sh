7c7e5f70d7ed4ec5e5947976d6ba2be38e430d31
Gustavo Verzola <verzola@gmail.com>
2022-06-18 17:08:02 -0300

    add shell to setup env vars to make wsl2 work with vagrant

---
+++ setup_wsl.sh
@@ -0,0 +1,3 @@
+export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
+export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="/mnt/c/Users/Gustavo\ Verzola/"
+export PATH="$PATH:/mnt/c/Program Files/Oracle/VirtualBox"
